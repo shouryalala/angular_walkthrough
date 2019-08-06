290db4989bb27537b435b911b46619085ad45e04
slala <shouryaditya.lala@blackrock.com>
2019-08-06 12:39:28 +0530

    week2 excercise 3, more services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
 //services
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
@@ -46,7 +48,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     AppRoutingModule
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
