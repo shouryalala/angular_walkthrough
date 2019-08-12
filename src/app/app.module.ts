88bf8ceb2fd25df2fdfee028644549ff5aa385e7
slala <shouryaditya.lala@blackrock.com>
2019-08-12 09:45:24 +0530

    week3 ex4 reactive forms, very diferent from template driven forms. Created a reactvie form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
+//reactive forms
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import 'hammerjs';
 
@@ -60,7 +64,10 @@ import { LoginComponent } from './login/login.component';
     MatInputModule,
     MatCheckboxModule,
     MatDialogModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     FormsModule
   ],
   entryComponents:[
